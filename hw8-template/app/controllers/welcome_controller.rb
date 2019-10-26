732add79bd057306e6c24f43df3a5c0f57f81f37
Zena Kipkenda <zenakipkenda@gmail.com>
2019-10-26 01:28:44 +0000

    Latest changes

--- hw8-template/app/controllers/welcome_controller.rb
+++
@@ -1,7 +0,0 @@
-class WelcomeController < ApplicationController
-  def index
-    @status = Status.new
-    return unless logged_in?
-    @statuses = Status.where(user: current_user.friends).or(Status.where(user: current_user))
-  end
-end
--- hw8-template/app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ApplicationRecord
-  has_many :friends, -> { where friendships: { state: 'accepted' } }, through: :friendships
-  has_many :requested_friends, -> { where friendships: { state: 'requested' } }, through: :friendships, source: :friend
-  has_many :pending_friends, -> { where friendships: { state: 'pending' } }, through: :friendships, source: :friend
-end
